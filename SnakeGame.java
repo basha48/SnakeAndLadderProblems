b2a0aa4b78f7ec27ec105dd24ec74debce9404e1
basha <bashamb787@gmail.com>
2022-01-22 12:47:24 +0530

    Find  Game Dice Number

--- SnakeGame.java
+++ SnakeGame.java
@@ -2,9 +2,12 @@ public class SnakeGame {
 
 	public static void main(String[] args) {
 		
-		
 		int Startposition =0;
-		System.out.println("Single person Start position is :" +Startposition);
+		
+		int  checknum =(int)(Math.random() * 6) + 1;
+		System.out.println("Single person Start position is " +Startposition);
+		System.out.println(" find dice number :" +checknum);
+
 
 	}
 }
