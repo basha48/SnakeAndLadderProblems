bcd699903efaed41db397b02ecbeb1c67693b1d0
basha <bashamb787@gmail.com>
2022-01-22 13:10:37 +0530

    palyer with options choosen

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,13 +1,34 @@
+
 public class SnakeGame {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 		
 		int Startposition =0;
 		
 		int  checknum =(int)(Math.random() * 6) + 1;
+		 int FindOptions= (int)(Math.random() * 2);
+		
+		
 		System.out.println("Single person Start position is " +Startposition);
-		System.out.println(" find dice number :" +checknum);
-
+		
+		 if (FindOptions==0) {
+			 System.out.println("player cannot be move because of Position " +FindOptions);
+			 
+		 }
+		 else if (FindOptions==1) {
+			 System.out.println("player  move  " +checknum+ ":steps up ");
+			 
+		 }
+		 
+		 else {
+			 System.out.println(" oops player has attacked snake   " +checknum+ ":steps down ");
+			 
+		 }
+		 
+		
 
 	}
+
 }
